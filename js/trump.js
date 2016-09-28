a3d5813be3e0ca38b2984a6de1fbdcee7f3a1800
Justen <justenbarget@gmail.com>
2016-09-28 15:37:35 +0100

    removing redundant html

--- js/trump.js
+++ js/trump.js
@@ -1,201 +1,222 @@
+var canvas = document.getElementById("canvas"),
+    animate = false,
+    rotations = 0,
+    continueAnimating = false,
+    score = 0;
 
 
-// var random = function(min, max) {
-//   return Math.floor(Math.random() * (max-min) + min);
-// };
-// //set up canvas defaults
-// var canvas = document.getElementById("canvas");
-// var ctx = canvas.getContext("2d");
-// var continueAnimating = {
-//   game: false
-// };
-// var score = 0;
-// var keydown = false;
-//
-// // draw dildos
-// var dildoNames = ['Beads', 'Black', 'Circ', 'Green', 'Mex', 'Pink', 'Tur', 'Uncir', 'Yellow'];
-// var dildoImagesArray = [];
-// for (var i = 0; i < dildoNames.length; i++) {
-//   dildoImagesArray[i] = new Image();
-//   dildoImagesArray[i].src = './assets/' + dildoNames[i]+'.png';
-// }
+var trumpImage = new Image(),
+    trumpHeight = 141,
+    trumpWidth = 112.5,
+    trumpSpeed = 10;
 
-var trumpImg = new Image();
-trumpImg.src = './assets/Trump.png';
+    trumpImage.src = "./assets/Trump.png";
 
+var totalDildos = 6,
+    dildoImagesArray = [],
+    activeDildos = [],
+    dildoNames = [
+      {name: 'Beads', width: 15, height: 75},
+      {name:'Black', width: 31, height: 80 },
+      {name:'Green', width: 13, height: 99 },
+      {name:'Mex', width: 42, height: 75 },
+      {name:'Pink', width: 26, height: 73 },
+      {name:'Red', width: 35, height: 75 },
+      {name:'Tur', width: 36, height: 75 },
+      {name:'Yellow', width: 38, height: 75 }
+    ];
 
-var trumpHeight = 542;
-var trumpWidth = 432;
-var trumpSpeed = 10;
+  // draw dildos
+ for (var i = 0; i < dildoNames.length; i++) {
+   dildoImagesArray[i] = {
+     image: new Image(),
+     dims: {
+       w: dildoNames[i].width,
+       h: dildoNames[i].height
+     }
+   };
+   dildoImagesArray[i].image.src = './assets/' + dildoNames[i].name +'.png';
+ }
 
-// var trump = {
-//   x:0,
-//   y: canvas.height - trumpHeight -112,
-//   width: trumpWidth,
-//   height: trumpHeight,
-//   speed: trumpSpeed
-// };
+function random(min, max) {
+  return Math.floor(Math.random() * (max-min) + min);
+}
 
-function sprite(options) {
-  var that = {};
+function gameLoop () {
+  if (continueAnimating) {
+    window.requestAnimationFrame(gameLoop);
+    trump.render();
+    trump.update();
+  }
+}
 
-  that.context = options.context;
-  that.width = options.width;
-  that.height = options.height;
-  that.speed = options.speed;
-  that.x = options.x;
-  that.y = options.y;
-  that.image = options.image;
+function sprite (options) {
+
+  var that = {},
+    frameIndex = 0,
+    tickCount = 0,
+    ticksPerFrame = options.ticksPerFrame || 0,
+    numberOfFrames = options.numberOfFrames || 1;
+
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.speed = options.speed;
+    that.x = options.x;
+    that.y = options.y;
+    that.image = options.image;
+
+  that.update = function () {
+    // for each dildo
+      // (1) check for collisions
+      // (2) advance the dildo
+      // (3) if the dildo falls below the canvas, end the game
+
+      for (var i = 0; i < activeDildos.length; i++) {
+        var dildo = activeDildos[i];
+        // test for dildo-trump collision
+        if (isColliding(dildo, trump)) {
+            score += 10;
+            eat();
+            resetDildo(dildo);
+        }
+        // advance the dildo
+        dildo.y += dildo.speed;
+
+        // if the dildo hits the canvas,
+        if (dildo.y + dildo.height > canvas.height - 110) {
+          continueAnimating = false;
+          document.getElementById('game-over').style.display = 'block';
+          document.getElementById('score').style.display = 'block';
+          document.getElementById('score').innerHTML = score.toString();
+          document.getElementById('start-button').style.display = 'block';
+          document.getElementById('start-button').style.bottom = '350px';
+          //show end screen
+        }
+      }
+
+    if (animate && rotations < 2){
+      tickCount += 1;
+      if (tickCount > ticksPerFrame) {
+        tickCount = 0;
+        // If the current frame index is in range
+        if (frameIndex < numberOfFrames - 1) {
+          // Go to the next frame
+          frameIndex += 1;
+        } else {
+          rotations += 1;
+          frameIndex = 0;
+        }
+      }
+    } else {
+      animate = false;
+      rotations = 0;
+    }
+  };
+
   that.render = function () {
-        // Draw the animation
-    that.image.onload = that.context.drawImage(
+    // Clear the canvas
+    that.context.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw the animation
+    that.context.drawImage(
       that.image,
+      frameIndex * that.width / numberOfFrames,
       0,
-      0,
-      that.width,
+      that.width / numberOfFrames,
       that.height,
       that.x,
       that.y,
-      that.width,
+      (that.width / numberOfFrames),
       that.height);
-    };
+      for (var i = 0; i < activeDildos.length; i++) {
+        var dildo = activeDildos[i];
+        dildo.onload = that.context.drawImage(dildoImagesArray[dildo.style].image ,dildo.x,dildo.y, dildoImagesArray[dildo.style].dims.w, dildoImagesArray[dildo.style].dims.h);
+      }
+      that.context.font = "14px Times New Roman";
+      that.context.fillStyle = "black";
+      that.context.fillText("Score: " + score, 10, 15);
+  };
 
   return that;
 }
 
+// Create sprite
 var trump = sprite({
   context: canvas.getContext("2d"),
-  image: trumpImg,
-  width: trumpWidth,
-  height: trumpHeight,
-  speed: trumpSpeed,
-  x: 0,
-  y: canvas.height - trumpHeight
+  width: 450,
+  height: 141,
+  image: trumpImage,
+  numberOfFrames: 4,
+  ticksPerFrame: 4,
+  x: canvas.width /2 - (0.5 * (trumpWidth)),
+  y: canvas.height - trumpHeight -110,
+  speed: trumpSpeed
 });
 
-trump.render();
+function eat(){
+  animate = true;
+  rotations = 0;
+}
 
+function addDildo() {
+  var number = random(0,8);
+  var dildo = {
+    style: number,
+    width: dildoImagesArray[number].dims.w,
+    height: dildoImagesArray[number].dims.h
+  };
 
-//
-// var dildoHeight = 100;
-// var dildoWidth = 50;
-// var totalDildos = 6;
-// var activeDildos = [];
+  resetDildo(dildo);
+  activeDildos.push(dildo);
+}
 
+function isColliding(a, b) {
+  return !(
+  b.x > a.x + a.width || b.x + (b.width/4) < a.x || b.y + 15 > a.y + a.height || b.y + b.height < a.y);
+}
 
-// for (var i = 0; i < totalDildos; i++) {
-//   addDildo();
-// }
-//
-// function addDildo() {
-//   var number = random(0,9);
-//   var dildo = {
-//     style: number,
-//     width: dildoWidth,
-//     height: dildoHeight
-//   };
-//
-//   resetDildo(dildo);
-//   activeDildos.push(dildo);
-// }
-//
-// function resetDildo(dildo) {
-//   dildo.x = Math.random() * (canvas.width - dildoWidth);
-//   dildo.y = 15 + Math.random() * 30;
-//   dildo.speed = 0.2 + Math.random() * 0.5;
-//   console.log(dildo);
-// }
-//
-// //left and right keypush event handlers
-// document.onkeydown = function (event) {
-//   if (event.keyCode == 39) {
-//     if (keydown) return;
-//     keydown = true;
-//     trump.x += trump.speed;
-//     if (trump.x + trump.width >= 398) {
-//       trump.x = 368;
-//     }
-//   } else if (event.keyCode == 37) {
-//     if (keydown) return;
-//     keydown = true;
-//     trump.x -= trump.speed;
-//     if (trump.x <= 0) {
-//       trump.x = 0;
-//     }
-//   }
-// };
-//
-// document.onkeyup = function(event) {
-//   if (event.keyCode == 39) {
-//     keydown = false;
-//   } else if (event.keyCode == 37) {
-//     keydown = false;
-//   }
-// };
-//
-// function animate(){
-//   if (continueAnimating.game) {
-//     requestAnimationFrame(animate);
-// }
-// // for each dildo
-//   // (1) check for collisions
-//   // (2) advance the dildo
-//   // (3) if the dildo falls below the canvas, reset that dildo
-//
-//   for (var i = 0; i < activeDildos.length; i++) {
-//     var dildo = activeDildos[i];
-//     // test for dildo-trump collision
-//     if (isColliding(dildo, trump)) {
-//         score += 10;
-//         animateTrump();
-//         resetDildo(dildo);
-//     }
-//     // advance the dildo
-//     dildo.y += dildo.speed;
-//
-//     // if the dildo hits the canvas,
-//     if (dildo.y + dildoHeight > canvas.height - 110) {
-//       continueAnimating.game = false;
-//       alert("Game Over: " + score);
-//     }
-//   }
-//
-//   function drawAll() {
-//
-//     // clear the canvas
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     background.onload = ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-//
-//     // draw the trump
-//     trumpImg.onload = trump.render();
-//     // draw all dildos
-//     for (var i = 0; i < activeDildos.length; i++) {
-//         var dildo = activeDildos[i];
-//         dildo.onload = ctx.drawImage(dildoImagesArray[dildo.style] ,dildo.x,dildo.y, dildoWidth, dildoHeight);
-//     }
-//
-//     // draw the score
-//     ctx.font = "14px Times New Roman";
-//     ctx.fillStyle = "black";
-//     ctx.fillText("Score: " + score, 10, 15);
-// }
-//
-//   // redraw everything
-//   drawAll();
-// }
-//
-// function isColliding(a, b) {
-//     return !(
-//     b.x > a.x + a.width || b.x + b.width < a.x || b.y > a.y + a.height || b.y + b.height < a.y);
-// }
-//
-// document.getElementById("start").addEventListener('click',function () {
-//     trump.x = 0;
-//     for (var i = 0; i < activeDildos.length; i++) {
-//         resetDildo(activeDildos[i]);
-//     }
-//     if (!continueAnimating.game) {
-//         continueAnimating.game = true;
-//         animate();
-//     }
-// });
+function resetDildo(dildo) {
+  dildo.style = random(0,8);
+  dildo.width = dildoImagesArray[dildo.style].dims.w;
+  dildo.height = dildoImagesArray[dildo.style].dims.h;
+  dildo.x = Math.random() * (canvas.width - dildo.width);
+  dildo.y = 0 - dildo.height -20;
+  dildo.speed = 0.2 + Math.random() * 0.5;
+}
+
+function moveDonaldRight(){
+  keydown = true;
+  trump.x += trump.speed;
+  if (trump.x + (trump.width/ 4) >= 360) {
+    trump.x = 360 - (trump.width/4);
+  }
+}
+
+function moveDonaldLeft(){
+  keydown = true;
+  trump.x -= trump.speed;
+  if (trump.x <= 0) {
+    trump.x = 0;
+  }
+}
+document.getElementById('right').addEventListener('click', moveDonaldRight);
+document.getElementById('left').addEventListener('click', moveDonaldLeft);
+
+// Load sprite sheet
+function start() {
+  setInterval(addDildo, 10000);
+  document.getElementById('info').style.display = 'none';
+  document.getElementById('game-over').style.display = 'none';
+  document.getElementById('left').style.display = 'inline-block';
+  document.getElementById('right').style.display = 'inline-block';
+  activeDildos = [];
+  score = 0;
+  for (var i = 0; i < totalDildos; i++) {
+    addDildo();
+  }
+  console.log('activeDildos',activeDildos);
+  if (!continueAnimating) {
+    continueAnimating = true;
+    gameLoop();
+  }
+}
